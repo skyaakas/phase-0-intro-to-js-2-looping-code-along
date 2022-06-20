4efed7553e52eee3262815bcc9b6cea42d0ec2be
+ <+>
2022-06-20 04:52:47 -0600

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solutions in this file
+
+
+
+//const newArray=["Guadalupe", "Ollie", "Aki"];
+
+
+
+
+
+function writeCards(newArray,newEvent) {
+  const newMessage = [];
+  for (let i = 0; i < newArray.length; i++) {
+    
+
+    const message =  `Thank you, ${newArray[i]}, for the wonderful ${newEvent} gift!`;
+    newMessage.push(message)
+    debugger;
+  }
+  return newMessage;
+  
+  
+}
+writeCards(["Guadalupe","Ollie","Aki"],"surprise");
+
+function countDown(i){
+while (i>=0) {
+  console.log(i);
+  i--;
+  
+}
+}
+countDown(10);
